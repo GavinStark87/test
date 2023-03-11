c41393d1df9fe5a23489d0d9e5ffca5e6bd7fb7a
Gavin Stark <gestark@ucsc.edu>
2023-03-10 18:41:19 -0800

    asd

---
+++ pa7/DictionaryTest.cpp
@@ -0,0 +1,34 @@
+/*
+Gavin Stark
+gestark
+pa7
+*/
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<stdexcept>
+#include"Dictionary.h"
+
+using namespace std;
+
+int main(){
+	Dictionary* D = new Dictionary();
+	D->setValue("a", 1);
+	D->setValue("b", 2);
+	D->setValue("c", 3);
+	Dictionary* F = D;
+	cout << F->equals(*D) << endl;
+	cout << F->to_string() << endl;
+	cout << D->size() << endl;
+	cout << D->contains("b") << endl;
+	D->begin();
+	cout << D->getValue("a") << endl;
+	cout << D->hasCurrent() << endl;
+	cout << D->currentKey() << endl;
+	cout << D->currentVal() << endl;
+	cout << D->to_string() << endl;
+	D->clear();
+	cout << D->to_string() << endl;
+	cout << F->to_string() << endl;
+
+}
